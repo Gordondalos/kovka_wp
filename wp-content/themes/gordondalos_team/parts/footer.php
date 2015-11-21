2d06fe32e8a29ddac9ce3a4b8159c62327c972c0
gordondalos <gordondalos@gmail.com>
2015-11-21 22:41:54 +0600

    поменял фавикон, поправил меню

--- wp-content/themes/gordondalos_team/parts/footer.php
+++ wp-content/themes/gordondalos_team/parts/footer.php
@@ -5,35 +5,35 @@
     </div>
     <ul id="js-navigation-menu" class="navigation-menu show">
         <!--                    <li class="nav-link"><a id="kompany" href="section_9">О Компании</a></li>-->
-        <li class="nav-link"><a id="kompany" href="company/">О Компании</a></li>
+        <li class="nav-link"><a id="kompany" href="<?php bloginfo('url'); ?>/company/">О Компании</a></li>
         <!--                    <li class="nav-link"><a id="raboti" href="section_10">Наши работы</a></li>-->
-        <li class="nav-link"><a id="raboti" href="job/">Наши работы</a></li>
+        <li class="nav-link"><a id="raboti" href="<?php bloginfo('url'); ?>/category/our-job/">Наши работы</a></li>
         <!--                    <li class="nav-link"><a id="foto" href="section_3">Фотогалерея</a></li>-->
-        <li class="nav-link"><a id="foto" href="gallery/">Фотогалерея</a></li>
+        <li class="nav-link"><a id="foto" href="<?php bloginfo('url'); ?>/category/gallery/">Фотогалерея</a></li>
         <!--                    <li class="nav-link"><a id="vid" href="section_6">Виды работ</a></li>-->
-        <li class="nav-link"><a id="vid" href="vid/">Виды работ</a></li>
+        <li class="nav-link"><a id="vid" href="<?php bloginfo('url'); ?>/vid/">Виды работ</a></li>
         <!--                    <li class="nav-link"><a id="contact" href="section_8">Контакты</a></li>-->
-        <li class="nav-link"><a id="contact" href="contact/">Контакты</a></li>
-        <li class="nav-link"><a href="prise/">Цены</a></li>
+        <li class="nav-link"><a id="contact" href="<?php bloginfo('url'); ?>/contact/">Контакты</a></li>
+        <li class="nav-link"><a href="<?php bloginfo('url'); ?>/prise/">Цены</a></li>
 
     </ul>
 
-    <div class="footer-secondary-links">
-        <ul>
-            <li><a href="javascript:void(0)">Terms and Conditions</a></li>
-            <li><a href="javascript:void(0)">Privacy Policy</a></li>
-        </ul>
-
-        <ul class="footer-social">
-            <li><a href="javascript:void(0)">
-                <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/facebook-logo-circle.png" alt="Facebook">
-            </a></li>
-            <li><a href="javascript:void(0)">
-                <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/twitter-logo-circle.png" alt="Twitter">
-            </a></li>
-            <li><a href="javascript:void(0)">
-                <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/youtube-logo-circle.png" alt="YouTube">
-            </a></li>
-        </ul>
-    </div>
+<!--    <div class="footer-secondary-links">-->
+<!--        <ul>-->
+<!--            <li><a href="javascript:void(0)">Terms and Conditions</a></li>-->
+<!--            <li><a href="javascript:void(0)">Privacy Policy</a></li>-->
+<!--        </ul>-->
+<!---->
+<!--        <ul class="footer-social">-->
+<!--            <li><a href="javascript:void(0)">-->
+<!--                <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/facebook-logo-circle.png" alt="Facebook">-->
+<!--            </a></li>-->
+<!--            <li><a href="javascript:void(0)">-->
+<!--                <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/twitter-logo-circle.png" alt="Twitter">-->
+<!--            </a></li>-->
+<!--            <li><a href="javascript:void(0)">-->
+<!--                <img src="https://raw.githubusercontent.com/thoughtbot/refills/master/source/images/youtube-logo-circle.png" alt="YouTube">-->
+<!--            </a></li>-->
+<!--        </ul>-->
+<!--    </div>-->
 </footer>
